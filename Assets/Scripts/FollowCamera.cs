334841c7208054fd18b22c43551c3e0a1e55d535
MSpaceDev <mspacedev1@gmail.com>
2018-10-23 22:49:23 +0200

    Initial commit

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+	public float smoothing = 1.0f;
+
+	public Transform target;
+	Vector3 offset;
+
+	// Use this for initialization
+	void Awake () {
+		offset = transform.position - target.position;
+	}
+
+	private void FixedUpdate()
+	{
+		Vector3 newTarget = target.position + offset;
+
+		transform.position = Vector3.MoveTowards(transform.position, newTarget, smoothing * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public bool IsWalkingLeft { get; set; }
+	public float moveSpeed;
+
+	public Transform groundCheck;
+	Rigidbody rb;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate()
+	{
+		transform.position += transform.forward * moveSpeed * Time.deltaTime;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+			Switch();
+
+		RaycastHit hit;
+
+		if(!Physics.Raycast(groundCheck.position, -transform.up, out hit, Mathf.Infinity))
+				anim.SetTrigger("isFalling");
+	}
+
+	private void Switch()
+	{
+		IsWalkingLeft = !IsWalkingLeft;
+
+		if (IsWalkingLeft)
+			transform.rotation = Quaternion.Euler(0,-45,0);
+		else
+			transform.rotation = Quaternion.Euler(0, 45, 0);
+	}
+}
